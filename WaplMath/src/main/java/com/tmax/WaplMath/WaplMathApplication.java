c9f7c226453ec1bb6794e6afd75ffaa0a5bee07c
sangheon_lee <sangheon_lee@tmax.co.kr>
2021-05-18 17:29:35 +0900

    First commit with project name WaplMath

---
+++ WaplMath/src/main/java/com/tmax/WaplMath/WaplMathApplication.java
@@ -0,0 +1,13 @@
+package com.tmax.WaplMath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaplMathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaplMathApplication.class, args);
+	}
+
+}
---
+++ WaplMath/src/test/java/com/tmax/WaplMath/WaplMathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmax.WaplMath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaplMathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
