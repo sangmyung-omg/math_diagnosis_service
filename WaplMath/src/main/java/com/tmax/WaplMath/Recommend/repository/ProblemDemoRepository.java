b5a75b3a8158010f7005f83953182048196190de
unknown <sangmyung_lee@tmax.co.kr>
2021-05-20 18:00:06 +0900

    feat: schema.sql added & name changes of DAOs applied to Repository files

--- WaplMath/src/main/java/com/tmax/WaplMath/Recommend/repository/ProblemDemoRepository.java
+++ WaplMath/src/main/java/com/tmax/WaplMath/Recommend/repository/ProblemDemoRepository.java
@@ -12,12 +12,12 @@ public interface ProblemDemoRepository extends CrudRepository<ProblemDemo, Strin
 	
 	ProblemDemo findByProbUuid(String probUuid);
 	
-	@Query("select distinct P.ukUuid from ProblemDemoDAO P where substr(P.chapter,1,14)=:sectionId")
+	@Query("select distinct P.ukUuid from ProblemDemo P where substr(P.chapter,1,14)=:sectionId")
 	List<String> findAllUkUuidBySection(@Param("sectionId") String sectionId);
 	
-	@Query("select distinct P.ukUuid from ProblemDemoDAO P where P.probTypeUuid=:typeUkId")
+	@Query("select distinct P.ukUuid from ProblemDemo P where P.probTypeUuid=:typeUkId")
 	List<String> findAllUkUuidByTypeUkId(@Param("typeUkId") String typeUkId);
 
-	@Query("select distinct P.ukUuid from ProblemDemoDAO P where substr(P.chapter,1,11) in :chapterList")
+	@Query("select distinct P.ukUuid from ProblemDemo P where substr(P.chapter,1,11) in :chapterList")
 	List<String> findAllUkUuidByChapterList(@Param("chapterList") List<String> chapterList);
 }
