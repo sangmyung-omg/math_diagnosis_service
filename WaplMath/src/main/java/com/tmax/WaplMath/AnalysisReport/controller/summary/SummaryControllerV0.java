c318f1ca27c7e55eb8411eed6f6d5a5c435ecdfa
성종현 <jonghyun_seong@tmax.co.kr>
2021-05-25 16:33:40 +0900

    [feat] impl summary service to controller

--- WaplMath/src/main/java/com/tmax/WaplMath/AnalysisReport/controller/summary/SummaryControllerV0.java
+++ WaplMath/src/main/java/com/tmax/WaplMath/AnalysisReport/controller/summary/SummaryControllerV0.java
@@ -1,6 +1,7 @@
 package com.tmax.WaplMath.AnalysisReport.controller.summary;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,16 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.tmax.WaplMath.AnalysisReport.config.Constants;
 import com.tmax.WaplMath.AnalysisReport.dto.SummaryReportDTO;
+import com.tmax.WaplMath.AnalysisReport.service.summary.SummaryServiceBase;
 
 @RestController
 @RequestMapping(path= Constants.apiPrefix + "/v0")
 class SummaryControllerV0 {
+    
     @Autowired
+    @Qualifier("v0")
+    private SummaryServiceBase summarySvc;
+
     @GetMapping("/summary")
     SummaryReportDTO getSummary(@RequestHeader("token") String token) {
-        SummaryReportDTO output =  new SummaryReportDTO();
-        output.setScore(100);
-        output.setPercentile(100.0);
-        return output;
+        //TODO: get userID and previledge from token
+        String userID = token;
+
+        return summarySvc.getSummaryOfUser(userID);
     }
 }
---
+++ WaplMath/src/main/java/com/tmax/WaplMath/AnalysisReport/service/summary/SummaryServiceV0.java
@@ -0,0 +1,20 @@
+package com.tmax.WaplMath.AnalysisReport.service.summary;
+
+import com.tmax.WaplMath.AnalysisReport.dto.SummaryReportDTO;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+@Qualifier("v0")
+public class SummaryServiceV0 implements SummaryServiceBase{
+    @Override
+    public SummaryReportDTO getSummaryOfUser(String userID) {
+        //FIXME: Dummy output
+        SummaryReportDTO output =  new SummaryReportDTO();
+        output.setScore(100);
+        output.setPercentile(100.0);
+
+        return output;
+    }
+}
