238c2c4ad383436af990e49e527d5797f118f7a3
성종현 <jonghyun_seong@tmax.co.kr>
2021-05-25 16:08:29 +0900

    [feat] Update basic chapter controller api

--- WaplMath/src/main/java/com/tmax/WaplMath/AnalysisReport/controller/chapter/ChapterControllerV0.java
+++ WaplMath/src/main/java/com/tmax/WaplMath/AnalysisReport/controller/chapter/ChapterControllerV0.java
@@ -2,11 +2,14 @@ package com.tmax.WaplMath.AnalysisReport.controller.chapter;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import com.tmax.WaplMath.AnalysisReport.config.Constants;
 import com.tmax.WaplMath.AnalysisReport.dto.ChapterDetailDTO;
 import com.tmax.WaplMath.AnalysisReport.dto.ChapterIDListDTO;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChapterControllerV0 {
     
     @GetMapping("/chapters")
-    List<ChapterDetailDTO> getChapterList(@RequestHeader("token") String token, @RequestBody ChapterIDListDTO chapterIDList){
+    List<ChapterDetailDTO> getChaptersList(@RequestHeader("token") String token){
+        return null;
+    }
+
+    @PostMapping("/chapters")
+    List<ChapterDetailDTO> getChaptersListFromChapterIDList(@RequestHeader("token") String token, 
+                                                            @RequestBody ChapterIDListDTO chapterIDList){
+        return null;
+    }
+
+    @GetMapping("/chapters/{userID}")
+    List<ChapterDetailDTO> getUserChaptersList(@RequestHeader("token") String token,
+                                               @PathParam("userID") String userID){
+        return null;
+    }
+
+    @PostMapping("/chapters/{userID}")
+    List<ChapterDetailDTO> getUserChaptersFromChapterIDList(@RequestHeader("token") String token, 
+                                                            @PathParam("userID") String userID,
+                                                            @RequestBody ChapterIDListDTO chapterIDList){
         return null;
     }
 }
