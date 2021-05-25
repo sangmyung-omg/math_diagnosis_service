c24c7313e5e4ec29486b4b353d2ee53736149b47
성종현 <jonghyun_seong@tmax.co.kr>
2021-05-25 15:52:41 +0900

    [feat] Add basic result controller

--- WaplMath/src/main/java/com/tmax/WaplMath/AnalysisReport/controller/result/ResultControllerV0.java
+++ WaplMath/src/main/java/com/tmax/WaplMath/AnalysisReport/controller/result/ResultControllerV0.java
@@ -1,9 +1,17 @@
 package com.tmax.WaplMath.AnalysisReport.controller.result;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.websocket.server.PathParam;
+
 import com.tmax.WaplMath.AnalysisReport.config.Constants;
 import com.tmax.WaplMath.AnalysisReport.dto.DiagnosisResultDTO;
+import com.tmax.WaplMath.AnalysisReport.dto.UserIDListDTO;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +24,19 @@ public class ResultControllerV0 {
         DiagnosisResultDTO output = new DiagnosisResultDTO();
 
         return output;
+    }
+
+    @GetMapping("/result/{userID}")
+    DiagnosisResultDTO getUserIDResult(@PathParam("userID") String userID) {
+        DiagnosisResultDTO output = new DiagnosisResultDTO();
+
+        return output;
+    }
+
+    @PostMapping("/results")
+    List<DiagnosisResultDTO> getUserIDResult(@RequestBody UserIDListDTO body) {
+        List<DiagnosisResultDTO> outputList = new ArrayList<DiagnosisResultDTO>();
+
+        return outputList;
     }
 }
