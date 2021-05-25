210b9429550be0d63be9d2656f1dee9c68bb8a52
성종현 <jonghyun_seong@tmax.co.kr>
2021-05-25 16:17:57 +0900

    [feat] Add chapterservice interface

---
+++ WaplMath/src/main/java/com/tmax/WaplMath/AnalysisReport/service/chapter/ChapterServiceBase.java
@@ -0,0 +1,12 @@
+package com.tmax.WaplMath.AnalysisReport.service.chapter;
+
+import java.util.List;
+
+import com.tmax.WaplMath.AnalysisReport.dto.ChapterDetailDTO;
+import com.tmax.WaplMath.AnalysisReport.dto.ChapterIDListDTO;
+
+public interface ChapterServiceBase {
+
+    List<ChapterDetailDTO> getAllChapterListOfUser(String userID);
+    List<ChapterDetailDTO> getSpecificChapterListOfUser(String userID, ChapterIDListDTO chapterIDList);
+}
