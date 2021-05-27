95149c165bc65a214f91c48aedb8a21854437eae
성종현 <jonghyun_seong@tmax.co.kr>
2021-05-27 14:52:26 +0900

    [feat] Add dummy data

--- WaplMath/src/main/java/com/tmax/WaplMath/AnalysisReport/service/result/ResultServiceV0.java
+++ WaplMath/src/main/java/com/tmax/WaplMath/AnalysisReport/service/result/ResultServiceV0.java
@@ -1,25 +1,55 @@
 package com.tmax.WaplMath.AnalysisReport.service.result;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
+import com.tmax.WaplMath.AnalysisReport.dto.ChapterIDListDTO;
 import com.tmax.WaplMath.AnalysisReport.dto.DiagnosisResultDTO;
 import com.tmax.WaplMath.AnalysisReport.dto.UserIDListDTO;
+import com.tmax.WaplMath.AnalysisReport.service.record.RecordServiceBase;
+import com.tmax.WaplMath.AnalysisReport.service.summary.SummaryServiceBase;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
 @Qualifier("v0")
 public class ResultServiceV0 implements ResultServiceBase{
+    @Autowired
+    private SummaryServiceBase summarySvc;
+
+    @Autowired
+    private RecordServiceBase recordSvc;
+
+
     @Override
     public List<DiagnosisResultDTO> getResultOfMultipleUsers(UserIDListDTO userIDList) {
-        // TODO Auto-generated method stub
-        return null;
+        List<DiagnosisResultDTO> resultList = new ArrayList<DiagnosisResultDTO>();
+        for(int i=0; i < userIDList.getUserIDList().size(); i++){
+            resultList.add(this.getResultOfUser(""));
+        }
+        return resultList;
     }
 
     @Override
     public DiagnosisResultDTO getResultOfUser(String userID) {
-        // TODO Auto-generated method stub
-        return null;
+        DiagnosisResultDTO resultData = new DiagnosisResultDTO();
+
+        resultData.setSummary(summarySvc.getSummaryOfUser(userID));
+        resultData.setLevelDiagnosisRecord(recordSvc.getRecordOfUser(userID));
+
+
+        ChapterIDListDTO chapterIDList = new ChapterIDListDTO();
+        List<String> idList = new ArrayList<String>();
+        for(int i=0; i<5; i++){
+            idList.add(UUID.randomUUID().toString());
+        }
+        chapterIDList.setUserIDList(idList);
+
+        resultData.setChapterIDList(chapterIDList);
+
+        return resultData;
     }
 }
