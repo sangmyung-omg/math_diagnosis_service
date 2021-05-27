791fefb52e416c8e7aa4654e6d785843ddc2bd36
성종현 <jonghyun_seong@tmax.co.kr>
2021-05-27 19:31:21 +0900

    [feat] Add dummy data services

---
+++ WaplMath/src/main/java/com/tmax/WaplMath/AnalysisReport/service/summary/SummaryServiceDummy.java
@@ -0,0 +1,20 @@
+package com.tmax.WaplMath.AnalysisReport.service.summary;
+
+import com.tmax.WaplMath.AnalysisReport.dto.SummaryReportDTO;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+@Qualifier("dummy")
+public class SummaryServiceDummy implements SummaryServiceBase{
+    @Override
+    public SummaryReportDTO getSummaryOfUser(String userID) {
+        //FIXME: Dummy output
+        SummaryReportDTO output =  new SummaryReportDTO();
+        output.setScore(100);
+        output.setPercentile(81.0);
+
+        return output;
+    }
+}
