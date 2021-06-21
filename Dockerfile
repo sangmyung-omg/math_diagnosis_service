9d03f6c823c6aab398a33ac85a79dff34a7dde82
이상헌 <sangheon_lee@tmax.co.kr>
2021-06-21 10:41:41 +0000

    feat: Add file logging, separate application.properties

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:8u111-jdk-alpine
 VOLUME /tmp
 ADD /WaplMath/build/libs/*.jar app.jar
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar", "/app.jar"]
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom", "-Dspring.profiles.active=prod", "-jar", "/app.jar"]
--- WaplMath/src/main/java/com/tmax/WaplMath/Recommend/config/RestfulConfig.java
+++ WaplMath/src/main/java/com/tmax/WaplMath/Recommend/config/RestfulConfig.java
@@ -5,10 +5,12 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@PropertySource("classpath:resttemplate.properties")
 public class RestfulConfig {
 
 	@Value("${restTemplate.factory.readTimeout}")
