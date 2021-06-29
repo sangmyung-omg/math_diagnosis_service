a71f6f90fee05a1f2d095a2627ec7f3578acc072
이상헌 <sangheon_lee@tmax.co.kr>
2021-06-29 10:26:17 +0000

    feat: Add triton waplscore host env

--- config/env.sh
+++ config/env.sh
@@ -28,6 +28,9 @@ sed -i "s/%TRITON_MODEL_NAME%/${TRITON_MODEL_NAME}/g" /home/tmax/triton.properti
 if [ -z $TRITON_MODEL_VER ]; then export TRITON_MODEL_VER="1"; fi
 sed -i "s/%TRITON_MODEL_VER%/${TRITON_MODEL_VER}/g" /home/tmax/triton.properties
 
+if [ -z $TRITON_WAPLSCORE_HOST ]; then export TRITON_WAPLSCORE_HOST="http://192.168.158.31:18000/v2/models/wapl-score/infer"; fi
+sed -i "s/%TRITON_WAPLSCORE_HOST%/${TRITON_WAPLSCORE_HOST}/g" /home/tmax/triton.properties
+
 if [ -z $LRS_IP ]; then export LRS_IP="192.168.153.132"; fi
 sed -i "s/%LRS_IP%/${LRS_IP}/g" /home/tmax/lrs.properties
 
