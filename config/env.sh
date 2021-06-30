369ee54a510d44fdbd18b5b62586005b63f4937f
sangheon_lee <sangheon_lee@tmax.co.kr>
2021-06-30 10:25:48 +0900

    feat: Add triton waplscore ip/port env variables

--- config/env.sh
+++ config/env.sh
@@ -28,8 +28,11 @@ sed -i "s/%TRITON_MODEL_NAME%/${TRITON_MODEL_NAME}/g" /home/tmax/triton.properti
 if [ -z $TRITON_MODEL_VER ]; then export TRITON_MODEL_VER="1"; fi
 sed -i "s/%TRITON_MODEL_VER%/${TRITON_MODEL_VER}/g" /home/tmax/triton.properties
 
-if [ -z $TRITON_WAPLSCORE_HOST ]; then export TRITON_WAPLSCORE_HOST="http://192.168.158.31:18000/v2/models/wapl-score/infer"; fi
-sed -i "s~%TRITON_WAPLSCORE_HOST%~${TRITON_WAPLSCORE_HOST}~g" /home/tmax/triton.properties
+if [ -z $TRITON_WAPLSCORE_IP ]; then export TRITON_WAPLSCORE_IP="192.168.158.31"; fi
+sed -i "s/%TRITON_WAPLSCORE_IP%/${TRITON_WAPLSCORE_IP}/g" /home/tmax/triton.properties
+
+if [ -z $TRITON_WAPLSCORE_PORT ]; then export TRITON_WAPLSCORE_PORT="18000"; fi
+sed -i "s/%TRITON_WAPLSCORE_PORT%/${TRITON_WAPLSCORE_PORT}/g" /home/tmax/triton.properties
 
 if [ -z $LRS_IP ]; then export LRS_IP="192.168.153.132"; fi
 sed -i "s/%LRS_IP%/${LRS_IP}/g" /home/tmax/lrs.properties
